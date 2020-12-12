56ef06005c5e0649507828e8146e24993a34b623
Kocha <kocha@Users-iMac.local>
2020-12-12 17:24:06 -0600

    add rest call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-rest';
+
+  restItems: any;
+  apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getRestItems();
+  }
+
+  // Read all REST Items
+  getRestItems(): void {
+    this.restItemsService()
+      .subscribe(
+        restItems => {
+          this.restItems = restItems;
+          console.log(this.restItems);
+        }
+      )
+  }
+
+
+  // Rest Items Service: Read all REST Items
+  restItemsService() {
+    return this.http
+      .get<any[]>(this.apiUrl)
+      .pipe(map(data => data));
+  }
 }
